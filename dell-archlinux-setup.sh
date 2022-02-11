ac0ddfe751d90fedb5fbc6e0aefea9de02170a55
Menny Even Danan <menny@evendanan.net>
2022-02-11 13:09:32 -0500

    dotfiles backup 2022-02-11 13:09:32

--- dell-archlinux-setup.sh
+++ dell-archlinux-setup.sh
@@ -19,9 +19,11 @@ makepkg -sri
 popd
 
 yay -S google-chrome
-yay -S bazelisk
-yay -S snapd
 
+yay -S bazelisk
+sudo ln -s $(which bazel) /usr/bin/bazelisk
+
+yay -S snapd
 sudo systemctl enable --now snapd.socket
 sudo ln -s /var/lib/snapd/snap /snap
 snap install snap-store
@@ -31,24 +33,31 @@ sudo echo "HibernateMode=shutdown" >> /etc/systemd/sleep.conf
 sudo echo "options snd-hda-intel model=auto" > /etc/modprobe.d/fix-audio-input.conf
 sudo echo "AutoEnable=true" >> /etc/bluetooth/main.conf
 
-sudo pacman -Sy fwupd gnome-firmware \
+sudo pacman -Syy fwupd gnome-firmware \
 	networkmanager wireless_tools \
 	bluez bluez-utils \
-	xorg-xwayland \
 	wmctrl xdotool imagemagick \
 	zsh zsh-completions \
 	cups cups-pdf
 chsh -s $(which zsh)
-#homectl update --shell=$(which zsh) "$ACTUAL_USER"
-sudo pacman -S nvidia nvidia-utils nvidia-settings xorg-server-devel opencl-nvidia
+#homectl update --shell=$(which zsh)
+sudo pacman -S nvidia nvidia-dkms \
+	libxnvctrl nvidia-utils nvidia-settings \
+	libvdpau opencl-nvidia \
+	ffnvcodec-headers \
+	xorg-server-devel xorg-xwayland egl-wayland \
+	primus_vk bumblebee mesa nvidia-prime
 sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
-sudo pacman -Sy gnome-themes-extra
+sudo gpasswd -a $USER bumblebee
+sudo systemctl enable bumblebeed.service
+
+sudo pacman -S gnome-themes-extra
+sudo pacman -S ttf-dejavu
 
 sudo systemctl start bluetooth.service
 sudo systemctl enable bluetooth.service
 
 sudo systemctl disable cups.service
-sudo systemctl start cups.socket
 sudo systemctl enable cups.socket
 
 sudo pacman -Syyu
