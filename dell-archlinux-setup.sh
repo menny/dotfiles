4980f98f30e104157d5d38ee65b77406f6a49970
Menny Even Danan <menny@evendanan.net>
2022-02-17 14:04:11 -0500

    dotfiles backup 2022-02-17 14:04:11

--- dell-archlinux-setup.sh
+++ dell-archlinux-setup.sh
@@ -9,9 +9,9 @@ fi
 
 USER_HOME_DIR="$(eval echo ~$USER)"
 
-sudo pacman -Sy archlinux-keyring
-sudo pacman -S --needed base-devel git
-sudo pacman -Sy openssl wget curl go jdk11-openjdk python3 clang
+sudo pacman -Syy archlinux-keyring
+sudo pacman -S --needed base-devel git git-lfs
+sudo pacman -S openssl wget curl go jdk11-openjdk python3 clang
 
 yay_git=$(mktemp -d)
 git clone https://aur.archlinux.org/yay-git.git "$yay_git"
@@ -19,7 +19,7 @@ pushd "$yay_git"
 makepkg -sri
 popd
 
-yay -S google-chrome
+yay -Syy google-chrome
 
 yay -S bazelisk
 sudo ln -s $(which bazel) /usr/bin/bazelisk
@@ -36,8 +36,10 @@ sudo echo "AutoEnable=true" >> /etc/bluetooth/main.conf
 
 sudo pacman -Syy fwupd gnome-firmware \
 	networkmanager wireless_tools \
+	usbutils \
 	bluez bluez-utils \
 	wmctrl xdotool imagemagick \
+	shellcheck \
 	zsh zsh-completions \
 	cups cups-pdf
 chsh -s $(which zsh)
@@ -48,9 +50,22 @@ sudo pacman -S nvidia nvidia-dkms \
 	ffnvcodec-headers \
 	xorg-server-devel xorg-xwayland egl-wayland \
 	primus_vk bumblebee mesa nvidia-prime
-sudo bash -c "echo blacklist nouveau > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
+sudo bash -c "echo 'blacklist nouveau' > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 sudo gpasswd -a $USER bumblebee
 sudo systemctl enable bumblebeed.service
+
+sudo echo "[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+Target=nvidia
+
+[Action]
+Description=Update Nvidia module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
+Exec=/usr/bin/mkinitcpio -P" > /etc/pacman.d/hooks/nvidia.hook
 
 sudo pacman -S gnome-themes-extra
 sudo pacman -S ttf-dejavu
@@ -61,7 +76,7 @@ sudo systemctl enable bluetooth.service
 sudo systemctl disable cups.service
 sudo systemctl enable cups.socket
 
-sudo pacman -Syyu
+sudo yay -Syyu
 
 git clone https://github.com/jenv/jenv.git ~/.jenv
 snap install slack --classic
